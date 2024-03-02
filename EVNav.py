52cc68a879251aeb59b5f9bd98fc7eb4ba4ffe09
mattraghu <mattraghu@aol.com>
2024-03-02 18:05:46 -0500

    Front End Input Pass Through

--- EVNav.py
+++ EVNav.py
@@ -6,14 +6,14 @@ with open("./APIKey.txt") as f:
 def EVEmissions(client, start, fin, battery):
     return battery
 
-def calculateOptimalEVRoute(client, start, fin, battery):
+def calculateOptimalEVRoute(client, start, end, battery):
     # Return a list of waypoints to be used in
-    start = (40.743462, -74.029068)
-    end = (42.886448, -78.878372)
+    # start = (40.743462, -74.029068)
+    # end = (42.886448, -78.878372)
 
     print("PRESSSED ")
 
-    ev_route_planner = EVRoutePlanner(api_key, start, end, 300000)  # 320 km range
+    ev_route_planner = EVRoutePlanner(api_key, start, end, int(battery)*1000) 
     route = ev_route_planner.calculate_route()
     
     waypoints = ev_route_planner.charging_stations
