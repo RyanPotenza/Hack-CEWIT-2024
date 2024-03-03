b2835d69d2c9dde202973954bf3c4eb7970631e5
Ryan Potenza <rpotenza@stevens.edu>
2024-03-03 06:59:21 -0500

    Added option for ML Nav

--- EVNav.py
+++ EVNav.py
@@ -4,12 +4,15 @@ with open("./APIKey.txt") as f:
     api_key = f.readline()
 
 
-def calculateOptimalEVRoute(client, start, end, battery):
+def calculateOptimalEVRoute(client, start, end, battery, method):
 
-    ev_route_planner = EVRoutePlanner(api_key, start, end, int(battery)*1000) 
-    route = ev_route_planner.calculate_route()
+    if method == 'default':
+        ev_route_planner = EVRoutePlanner(api_key, start, end, int(battery)*1000) 
+        route = ev_route_planner.calculate_route()
+        waypoints = ev_route_planner.charging_stations
+    else:
+        return [], 0.0
     
-    waypoints = ev_route_planner.charging_stations
 
     return waypoints, round(ev_route_planner.emissions_kg_co2, 3)
 
