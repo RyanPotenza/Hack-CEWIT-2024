f351baaf22767592edacf1c5a76cd2e1b00a9403
Ryan Potenza <rpotenza@stevens.edu>
2024-03-03 04:16:27 -0500

    Formatting and presentation data

--- EVNav.py
+++ EVNav.py
@@ -5,17 +5,11 @@ with open("./APIKey.txt") as f:
 
 
 def calculateOptimalEVRoute(client, start, end, battery):
-    # Return a list of waypoints to be used in
-    # start = (40.743462, -74.029068)
-    # end = (42.886448, -78.878372)
-
-    print("PRESSSED ")
 
     ev_route_planner = EVRoutePlanner(api_key, start, end, int(battery)*1000) 
     route = ev_route_planner.calculate_route()
     
     waypoints = ev_route_planner.charging_stations
-    # waypoints = [{'lat': 40.743462, 'lng': -74.029068}, {'lat': 42.886448, 'lng': -78.878372}]
 
     return waypoints, round(ev_route_planner.emissions_kg_co2, 3)
 
